d866a1ddfa06af0f1715149a9202a6e353a667e7
PavelReshetnev <pasha8804088@gmail.com>
2023-03-04 13:33:11 +0700

    Initial commit

---
+++ ConsoleApp6/Calculation.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp6;
+
+public class Calculation
+{
+    public string calculationLine;
+
+    public string Calc
+    {
+        get => this.calculationLine;
+        set => this.calculationLine = value;
+    }
+
+    
+    public void SetCalculationLine()
+    {
+        calculationLine = Console.ReadLine();
+    }
+
+    public void SetLastSymbolCalculationLine()
+    {
+        calculationLine += Console.ReadLine();
+    }
+
+    public void GetCalculationLine()
+    {
+        Console.WriteLine($"Значения calculationLine = {calculationLine}");
+    }
+
+    public void GetLastSymbol()
+    {
+        Console.WriteLine($"Последний символ:{calculationLine[calculationLine.Length - 1]}");
+    }
+
+    public void DeleteLastSymbol()
+    {
+        int x = calculationLine.Length - 1;
+        calculationLine = calculationLine.Remove(x);
+        Console.WriteLine($"Число удалено...");
+        Console.WriteLine($"Значения calculationLine = {calculationLine}");
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp6;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Calculation solve = new Calculation();
+        Console.WriteLine("Введите значения calculationLine");
+        solve.SetCalculationLine();
+        Console.WriteLine("Изменено...\nДобавте новый символ");
+        solve.SetLastSymbolCalculationLine();
+        Console.WriteLine("Изменено...\n");
+        solve.GetCalculationLine();
+        solve.GetLastSymbol();
+        Console.WriteLine("");
+        solve.DeleteLastSymbol();
+    }
+}
